4c30cdf9e9aaf076cdaa68d577f852291e3df022
mrarthurwhite <mister.arthur.white@gmail.com>
2020-04-06 14:17:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+function findMatching(drivers , keyword){
+  let matchingDrivers = [];
+  matchingDrivers = drivers.filter( driver=>{
+    return driver.toLowerCase() == keyword.toLowerCase();
+  });
+  return matchingDrivers;
+}
+
+function fuzzyMatch(drivers , keyword){
+  let matchingDrivers = drivers.filter( driver=>{
+    return driver.startsWith(keyword);
+  });
+  return matchingDrivers;
+}
+
+function matchName(drivers , keyword){
+  let matchingDrivers = drivers.filter( driver=>{
+    return driver.name==keyword;
+  });
+  return matchingDrivers;
+}
+
+
